ee0cf828cea52ee90e01b6e4312f85aea54d6009
romainmarinier <60342572+romainmarinier@users.noreply.github.com>
2020-05-29 18:30:25 +0200

    Create index.php

---
+++ site/index.php
@@ -0,0 +1,19 @@
+            <?php
+            $page="accueil";
+            include 'header.inc.php';
+            ?>
+
+     		<div class="conteneur1">
+        		<div class="bloc_principal">
+        	</div></div>
+
+        	<footer>
+        		<div id="Bloc_entier">
+        			<div id="Bloc_gauche"> <img src="Chronometre.png">&emsp;INFOS DERNIERES MINUTES</div>
+
+        			<div id="Bloc_droite">Frederic reprend le club</div>
+
+        		</div>
+        	</footer>
+    </body>
+</html>
