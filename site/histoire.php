42b3ac39c7c946fde741b923545e2535f1c104ec
romainmarinier <60342572+romainmarinier@users.noreply.github.com>
2020-05-29 18:29:31 +0200

    Create histoire.php

---
+++ site/histoire.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+         <link rel="stylesheet" href="Css/histoire.css" />
+        <title>Atlético Nantais - Site officiel</title>
+    </head>
+
+    <body>
+            <header>
+			<div id="conteneur1">
+        		<div id="logo">
+        			<div class="le_logo"> <p> <img src="/Users/romainmarinier/Documents/GitHub/MP_SFL_2019_G1_At_Nantes/Photos présentes sur site/Logo Atlético nantais.png"> </p> </div>
+        		</div>
+
+        		<nav>
+        		<div id="menu_naviguant">
+        			<ul>
+        				<li><a href="#">RESULTATS/CALENDRIER</a></li>
+        				<li><a href="#">EFFECTIF PRO</a></li>
+        				<li><a href="#">HISTORIQUE</a></li>
+        				<li><a href="#">PHOTOS</a></li>
+        			</ul>
+        		</div>
+        		</nav>
+        	</div>
+
+        	<div id="conteneur2">
+        		<div class="text">HISTORIQUE</div>
+        	</div>
+
+        	</header>
+    
+    <body>
+  
+  <section>
+       
+              <div id="DATES">
+                    <h2 class="rouge">LES DATES CLÉS</h2>
+                    
+                    <p>Depuis le début du XXe siècle, Reims et football de haut niveau sont indissociables. </br> Une histoire singulière, parfois romanesque, jalonnée d'exploits, d'épopées et parfois même de périodes de disette.
+                    </br> Premier géant du football hexagonal, le Stade de Reims a traversé les époques et se tourne vers l'avenir
+                    </br>, au masculin comme au féminin. Un club, une légende, une institution...
+                </p>
+    
+                </div>
+    </section>
+    <section>
+
+                <div id="section2">
+                    <h2>The First Dream Team !!!</h2>
+                    <p></p>
+                    <p>L'atletico Nantais prend une nouvelle dimension à la suite de sa fusion avec le club rival régional à la tunique rouge et blanche, le Sporting Club Nantais. Chacun apporte quelque chose en dot à ce « mariage » : c’est le nom pour le Atletico Nantais et les couleurs pour le Sporting ! Bienvenue au maillot rouge à manches blanches qui donnera sa vraie identité a L'atletico Nantais et marquera l’histoire du football en France et en Europe.</p>
+</div>
+</section>
+<section>                
+                <div id="section3">
+                    <h2>Premier titre</h2>
+                    <p>Après avoir décroché le titre de champion de France amateur en 1935, le club évolue, est promu en deuxième division et devient un club de football professionnel. Pour sa première participation, il termine onzième du championnat.</p>
+                   
+                 </div>
+</section>
+<section> 
+
+                    <div id="section4">
+                    <h2>Première Coupe de France</h2>
+                    <p>Après le Championnat, pourquoi pas la Coupe ? Le SDR remporte la Coupe de France pour la première fois en s’imposant face au RC Paris 2-0… deux buts inscrits dans les 10 dernières minutes de la rencontre !</p>
+                   
+                 </div>
+ </section>
+<section>                 
+                 <div id="section5">
+                    <h2>Premier titre</h2>
+                    <p>Après avoir décroché le titre de champion de France amateur en 1935, le club évolue, est promu en deuxième division et devient un club de football professionnel. Pour sa première participation, il termine onzième du championnat.</p>
+                   
+                    </div>
+</section>
+<section>                  
+                    <div id="section6">
+                    <h2>Premier titre</h2>
+                    <p>Après avoir décroché le titre de champion de France amateur en 1935, le club évolue, est promu en deuxième division et devient un club de football professionnel. Pour sa première participation, il termine onzième du championnat.</p>
+
+                   
+                   </div>
+ </section>               
+                <aside>
+                    <h1>À propos de l'equipe</h1>
+
+                    <p>L'equipe etait composer de : </p>
+                    <p>(GB) kawazaki</p
+                    <p>(DEF) Nakamoto </p>
+                </aside>
+            
