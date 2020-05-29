5f672a920f64f9e0934893710a19407e9266304c
romainmarinier <60342572+romainmarinier@users.noreply.github.com>
2020-05-29 19:34:05 +0200

    Update footer.inc.php

---
+++ site/footer.inc.php
@@ -0,0 +1,10 @@
+<footer>
+        		<div id="Bloc_entier">
+        			<div id="Bloc_gauche"> <img src="img/Chronometre.png">&emsp;INFOS DERNIERES MINUTES</div>
+
+        			<div id="Bloc_droite">Frederic reprend le club</div>
+
+        		</div>
+        	</footer>
+    </body>
+</html>
