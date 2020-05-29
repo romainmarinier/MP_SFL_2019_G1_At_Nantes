0a5ad0d2b4762c41db15aec976dadbf507fe8868
romainmarinier <60342572+romainmarinier@users.noreply.github.com>
2020-05-29 19:33:46 +0200

    Update index.php

--- site/index.php
+++ site/index.php
@@ -7,13 +7,6 @@
         		<div class="bloc_principal">
         	</div></div>
 
-        	<footer>
-        		<div id="Bloc_entier">
-        			<div id="Bloc_gauche"> <img src="Chronometre.png">&emsp;INFOS DERNIERES MINUTES</div>
-
-        			<div id="Bloc_droite">Frederic reprend le club</div>
-
-        		</div>
-        	</footer>
-    </body>
-</html>
+            <?php
+            include 'footer.inc.php';
+            ?>
