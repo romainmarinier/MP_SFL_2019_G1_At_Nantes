903d4da53fba86ad1a4b88f4a1d463c629315b14
Reddy35500 <leuwhugo@gmail.com>
2020-06-15 15:43:36 +0200

    Update histoire.php

--- site/histoire.php
+++ site/histoire.php
@@ -1,7 +1,29 @@
-            <?php
-            $page="Historique";
-            include 'header.inc.php';
-            ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+         <link rel="stylesheet" href="histoire.css" />
+        <title>Atlético Nantais - Site officiel</title>
+    </head>
+
+    <body>
+            <header>
+			<div id="conteneur1">
+        		<div id="logo">
+        			<div class="le_logo"> <p> <img src="/Users/romainmarinier/Documents/GitHub/MP_SFL_2019_G1_At_Nantes/Photos présentes sur site/Logo Atlético nantais.png"> </p> </div>
+        		</div>
+
+        		<nav>
+        		<div id="menu_naviguant">
+        			<ul>
+        				<li><a href="#">RESULTATS/CALENDRIER</a></li>
+        				<li><a href="#">EFFECTIF PRO</a></li>
+        				<li><a href="#">HISTORIQUE</a></li>
+        				<li><a href="#">PHOTOS</a></li>
+        			</ul>
+        		</div>
+        		</nav>
+        	</div>
 
         	<div id="conteneur2">
         		<div class="text">HISTORIQUE</div>
@@ -14,10 +36,10 @@
   <section>
        
               <div id="DATES">
-                    <h2 class="rouge">LES DATES CLÉS</h2>
+                    <h2>LES DATES CLÉS</h2>
                     
-                    <p>Depuis le début du XXe siècle, Reims et football de haut niveau sont indissociables. </br> Une histoire singulière, parfois romanesque, jalonnée d'exploits, d'épopées et parfois même de périodes de disette.
-                    </br> Premier géant du football hexagonal, le Stade de Reims a traversé les époques et se tourne vers l'avenir
+                    <p>Depuis le début du XXe siècle, Nantes et football de haut niveau sont indissociables. </br> Une histoire singulière, parfois romanesque, jalonnée d'exploits, d'épopées et parfois même de périodes de disette.
+                    Premier géant du football hexagonal, </br>l'Atletico Nantais a traversé les époques et se tourne vers l'avenir
                     </br>, au masculin comme au féminin. Un club, une légende, une institution...
                 </p>
     
@@ -34,7 +56,9 @@
 <section>                
                 <div id="section3">
                     <h2>Premier titre</h2>
-                    <p>Après avoir décroché le titre de champion de France amateur en 1935, le club évolue, est promu en deuxième division et devient un club de football professionnel. Pour sa première participation, il termine onzième du championnat.</p>
+                    <p>Après avoir décroché le titre de champion de France amateur en 1935, le club évolue, est promu en deuxième division et devient un club de football professionnel. Pour sa première participation, il termine onzième du championnat.</br>
+
+                    Le 4 mai est une date particulière pour tous les fans de l'tletico nantais. Il y a exactement 17 ans, le club remportait le premier titre de champion de France de son histoire en s'imposant face au RC Lens au Stade Gerland, dans un match capital.</p>
                    
                  </div>
 </section>
@@ -48,28 +72,21 @@
  </section>
 <section>                 
                  <div id="section5">
-                    <h2>Premier titre</h2>
-                    <p>Après avoir décroché le titre de champion de France amateur en 1935, le club évolue, est promu en deuxième division et devient un club de football professionnel. Pour sa première participation, il termine onzième du championnat.</p>
+                    <h2>Le roi Ronnie</h2>
+                    <p>Il y a aujourd'hui 18 ans jour pour jour, l'atletico nantais annonçait avoir obtenu la signature du jeune espoir brésilien Ronaldinho. Un accord qui avait nécessité l'intervention de la justice brésilienne pour être entériné, son club formateur du Grêmio refusant de le laisser partir.</p>
                    
                     </div>
 </section>
 <section>                  
                     <div id="section6">
-                    <h2>Premier titre</h2>
-                    <p>Après avoir décroché le titre de champion de France amateur en 1935, le club évolue, est promu en deuxième division et devient un club de football professionnel. Pour sa première participation, il termine onzième du championnat.</p>
+                    <h2>Premier LDC</h2>
+                    Ça s’est passé un 19 Mai… celui de 2012 ! l'atletico Nantais remportait pour la première fois de son histoire la Ligue des Champions face au Bayern au stade de l’Allianz Arena à Munich (1-1, 4-3 tab).
+                    <p>
 
                    
                    </div>
  </section>               
                 <aside>
                     <h1>À propos de l'equipe</h1>
-
-                    <p>L'equipe etait composer de : </p>
-                    <p>(GB) kawazaki</p>
-                    <p>(DEF) Nakamoto </p>
                 </aside>
-
-            <?php
-            include 'footer.inc.php';
-            ?>
             
