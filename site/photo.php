8645c86dc34e24ae0010146adf5c953ca0a1e008
romainmarinier <60342572+romainmarinier@users.noreply.github.com>
2020-06-18 00:49:46 +0200

    Update photo.php

--- site/photo.php
+++ site/photo.php
@@ -16,51 +16,43 @@
 
             <div id="presentation">La saison 2019-2020 s'est achevée au stade Raymond-Kopa face à RENNES dans le cadre de la 38ème journée de L1. <br/> Auteurs d'une première partie du tableau encourageante, les coéquipiers d'Abdoulaye Touré terminent finalement en <br/>13ème position à dix journées du terme d'un parcours tronqué par la pandémie du  Covid-19. <br/>atleticonantais.com vous propose une rétrospective en images des instants clés qui ont jalonné le parcours 2019-2020 <br/>des "Rouges-et-Noirs".</div>
 
-            <div id="bestof">
-                <div class="soustitre">&nbsp;LANCEMENT DE LA SAISON</div>
-                <div class="resume">&ensp;L'Atletico Nantais entame sa 1ère saison en L1 avec la réception de Nimes.</div>
-            </div>
-            
-            <div id="bestof2">
-                <div class="soustitre">&nbsp;L'Atletico Nantais voit double !</div>
-                <div class="resume">&ensp;Après Metz, l'Atletico Nantais enchaine à domicile contre Amiens.</div>
-            </div>
+            <div id="bdd">
+                
+                        <?php
+                        try
+                        {
+        // On se connecte à MySQL
+                            $bdd = new PDO('mysql:host=localhost;dbname=atletico;charset=utf8', 'atleticouser', 'Nantes44');
+                        }
+                        catch(Exception $e)
+                        {
+        // En cas d'erreur, on affiche un message et on arrête tout
+                            die('Erreur : '.$e->getMessage());
+                        }
 
-            <div id="bestof3">
-                <div class="soustitre">&nbsp;La troisième marche !</div>
-                <div class="resume">&ensp;L'Atletico Nantais est sur le podium: 10 matchs sans défaites consécutifs.</div>
-            </div>
+    // Si tout va bien, on peut continuer
 
-            <div id="bestof4">
-                <div class="soustitre">&nbsp;Fin de série</div>
-                <div class="resume">&ensp;Après 14 matchs sans défaites, l'Atlético Nantais s'incline 2-0 contre Paris.</div>
-            </div>
+    // On récupère tout le contenu de la table score
+                        $reponse = $bdd->query('SELECT * FROM photo');
 
-            <div id="bestof5">
-                <div class="soustitre">&nbsp;Caillard : un match fou</div>
-                <div class="resume">&ensp;Contre Angers, le gardien égalise à la 95ème minute d'une tête sur corner!</div>
-            </div>
+    // On affiche chaque entrée une à une
 
-            <div id="bestof6">
-                <div class="soustitre">&nbsp;Le miracle de la VAR</div>
-                <div class="resume">&ensp;La VAR sauve l'Atlético Nantais d'une défaite à Lyon dans les arrets de jeu.</div>
-            </div>
-
-            <div id="bestof7">
-                <div class="soustitre">&nbsp;On n'arrête plus l' Atletico Nantais</div>
-                <div class="resume">&ensp;À 2 journées de la fin, l'Atletico Nantais est 1er avec 4 points d'avance.</div>
-            </div>
-
-            <div id="bestof8">
-                <div class="soustitre">&nbsp;Champion de France !!!</div>
-                <div class="resume">&ensp;L'Atletico Nantais est champion de France ! Historique !</div>
-            </div>
+                        while ($donnees = $reponse->fetch())
+                        {
+                            ?>
+                            <div id="bestof">
+                                <img src="img/<?php echo $donnees['image']; ?>" />
+                                 <div class="soustitre">&nbsp;<?php echo $donnees['soustitre']; ?></div>
+                                <div class="resume">&ensp;<?php echo $donnees['resume']; ?></div>
+                            </div>
+                            <?php
+                        }
 
 
-            <div id="conclusion">L'Atletico Nantais remercie tous ses supporters pour leur soutien sans faille dans les bons comme dans les mauvais moments.<br/> Le club espère vivre une saison 2020-2021 à la hauteur de vos espérances.</div>
+    $reponse->closeCursor(); // Termine le traitement de la requête
 
+    ?>
 
-            </div>
 
            
 
